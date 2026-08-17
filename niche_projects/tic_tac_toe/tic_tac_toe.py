2acb0ef4078234f07bdbdaa40accc647ba7c812a
Afnan-AE <afnanisneutral16@gmail.com>
2026-08-17 18:07:24 +0600

    made the standard base and still no repitition

--- niche_projects/tic_tac_toe/tic_tac_toe.py
+++ niche_projects/tic_tac_toe/tic_tac_toe.py
@@ -1,1 +1,49 @@
-print("Hello Worlds")
+import random
+
+table_3x3 = [[' ',' ',' '],
+                              [' ',' ',' '],
+                              [' ',' ',' ']]
+
+
+#function to print the table in each try instance
+def print_table(table):
+    print("",end='\n\n')
+    for i in range(3):
+        for j in range(3):
+            if j == 2 : print(f" {table_3x3[i][j]}")
+            else : print(f" {table_3x3[i][j]} ", end='|')
+        if i != 2 : print("-----------",end= '\n')
+    print("",end='\n\n')
+
+
+#function for bot to choice
+def random_choicer():
+    random_row = random.randint(0,2)
+    random_colm = random.randint(0,2)
+    return random_row, random_colm
+
+
+print_table(table_3x3)
+
+
+my_choice = [None, None]
+my_choice[0] = int(input("Enter row: "))
+my_choice[1] = int(input("Enter coloumn: "))
+while(table_3x3[my_choice[0]-1][my_choice[1]-1] != ' '):
+    print("THIS SPACE IS TAKEN !! ", end='\n')
+    my_choice[0] = int(input("Enter row: "))
+    my_choice[1] = int(input("Enter coloumn: "))
+
+print("",end="\n")
+
+table_3x3[my_choice[0]-1][my_choice[1]-1] = 'O'
+
+#loop for bot to not choose the taken square
+choice_row_bot,choice_colm_bot = random_choicer()
+while(table_3x3[choice_row_bot][choice_colm_bot] != ' '):
+    choice_row_bot,choice_colm_bot = random_choicer()
+
+table_3x3[choice_row_bot][choice_colm_bot] = 'X'
+
+
+print_table(table_3x3)
