aecd454ec9c1982caa1634172ff2b88a40c66ee8
Afnan-AE <afnanisneutral16@gmail.com>
2026-09-02 02:51:34 +0600

    Added logo, added input boundaries only for option choice

--- niche_projects/to_do_list/to_do_list.py
+++ niche_projects/to_do_list/to_do_list.py
@@ -10,8 +10,17 @@ def task_viewer():
     if size_ == 0: print("There are no tasks add some!\n")
     else:
         for i in range(size_):
-            print(f"{i+1}. {data['tasks'][str(i+1)]}")
+            print(f"{i+1}# {data['tasks'][str(i+1)]}")
     
+def print_logo():
+    print("\n")
+    print(r"                             ┏━ ┏━┃  ┏━ ┏━┛    ")
+    print(r"                             ┃ ┃┃ ┃  ┏━┃━━┃    ")
+    print(r"                             ┛ ┛━━┛  ━━ ━━┛    ")
+    print(r"                        ━┏┛┏━┃┏━ ┏━┃  ┃  ┛┏━┛━┏┛")
+    print(r"                         ┃ ┃ ┃┃ ┃┃ ┃  ┃  ┃━━┃ ┃ ")
+    print(r"                         ┛ ━━┛━━ ━━┛  ━━┛┛━━┛ ┛ ")
+    print("\n")
 
 def task_adder(stri):
     with open('memory.json', 'r') as f:
@@ -50,13 +59,19 @@ def deleter_task():
     del_task(i)
     del_last_task()
 
+
 run_state = True
 lock_key = False
+lock_keyx = False
 while(run_state):
+    with open('memory.json') as f:
+        data = json.load(f)
+    nt = len(data['tasks'])
+    
     if lock_key : os.system("clear")
 
-    print('\n--------------------------------NO BS TODO LIST-------------------------------\n')
-    print('     Print Todo list [1]     Add task [2]      Complete task[3]    Quit [q]\n')
+    print_logo()
+    print('Print Todo list [1]     Add task [2]      Complete task [3]    Quit [q]\n')
 
     if lock_key : 
         print()
@@ -64,19 +79,27 @@ while(run_state):
         print()
         lock_key = False
 
-    ans = input("What do you want to?: ")
-    if ans == "1":lock_key = True
-    elif ans == "2":
-        task = input("\nSpill the tea!: ")
-        task_adder(task)
-        os.system("clear")
-    elif ans == "3": 
+    if lock_keyx :
+        print()
+        task_viewer()
         deleter_task()
         os.system("clear")
-    elif ans == "q": 
-        run_state = False
-        print()
-        print("Come by again <3")
-        print()
-        time.sleep(4)
-        os.system("clear")
+        lock_keyx = False
+    else:
+        ans = input("What do you want to?: ")
+        while(len(ans) != 1) or ((ans<"1" or ans>"4") and (ans<"q" or ans > "q")):
+            ans = input("What do you want to?: ")
+                  
+        if ans == "1":lock_key = True
+        elif ans == "2":
+            task = input(f"\n Task no #{nt+1} :  ")
+            task_adder(task)
+            os.system("clear")
+        elif ans == "3": lock_keyx = True
+        elif ans == "q": 
+            run_state = False
+            print()
+            print("Come by again <3")
+            print()
+            time.sleep(4)
+            os.system("clear")
