c3d07a00d1f28d3d54518d426a5e5aea00147768
Afnan-AE <afnanisneutral16@gmail.com>
2026-09-02 19:20:54 +0600

    Added a few stuff, but messed up the code around the completing task abit

--- niche_projects/to_do_list/to_do_list.py
+++ niche_projects/to_do_list/to_do_list.py
@@ -1,7 +1,12 @@
 import json
-import os
-import time
+from os import system,name # to handle clearning the screen
+from time import sleep # to sleep
 
+#func to clear screen in any environment
+def clear_screen():
+    system("cls" if name == "nt" else "clear")
+
+# func to print the todo list
 def task_viewer():
     with open('memory.json','r') as f:
         data = json.load(f)
@@ -10,8 +15,9 @@ def task_viewer():
     if size_ == 0: print("There are no tasks add some!\n")
     else:
         for i in range(size_):
-            print(f"{i+1}# {data['tasks'][str(i+1)]}")
-    
+            print(f"|{i+1}|- {data['tasks'][str(i+1)]} -|{i+1}|")
+
+# func to print the logo
 def print_logo():
     print("\n")
     print(r"                             ┏━ ┏━┃  ┏━ ┏━┛    ")
@@ -22,6 +28,7 @@ def print_logo():
     print(r"                         ┛ ━━┛━━ ━━┛  ━━┛┛━━┛ ┛ ")
     print("\n")
 
+# func to add task 1 by 1
 def task_adder(stri):
     with open('memory.json', 'r') as f:
         data = json.load(f)
@@ -30,7 +37,8 @@ def task_adder(stri):
 
     with open('memory.json', 'w') as f:
         json.dump(data, f)
-    
+
+# func to delete the last task in the todo list
 def del_last_task():
     with open('memory.json', 'r') as f:
         data = json.load(f)
@@ -40,6 +48,7 @@ def del_last_task():
     with open('memory.json','w') as f:
         json.dump(data, f)
 
+# func to delete selected task
 def del_task(tn):
     with open('memory.json', 'r') as f:
         data = json.load(f)
@@ -54,52 +63,78 @@ def del_task(tn):
     with open('memory.json','w') as f:
         json.dump(data, f)
 
+# overall func to handle task deleting
 def deleter_task():
-    i = int(input('\nWhich task have you completed?: '))
-    del_task(i)
-    del_last_task()
-
-
-run_state = True
-lock_key = False
-lock_keyx = False
-while(run_state):
-    with open('memory.json') as f:
+    with open('memory.json','r') as f:
         data = json.load(f)
-    nt = len(data['tasks'])
-    
-    if lock_key : os.system("clear")
-
-    print_logo()
-    print('Print Todo list [1]     Add task [2]      Complete task [3]    Quit [q]\n')
-
-    if lock_key : 
-        print()
-        task_viewer()
-        print()
-        lock_key = False
-
-    if lock_keyx :
-        print()
-        task_viewer()
-        deleter_task()
-        os.system("clear")
-        lock_keyx = False
+    s = str(len(data['tasks']))
+    print("",end="\n")
+    i = input('Which task have you completed?: ')
+    if i == "all":
+        t=int(s)
+        while(t):
+            del_last_task()
+            t-=1
+        pass
     else:
-        ans = input("What do you want to?: ")
-        while(len(ans) != 1) or ((ans<"1" or ans>"4") and (ans<"q" or ans > "q")):
+        while(i<"1" or i>s):
+            i = input('Which task have you completed?: ')
+
+        del_task(int(i))
+        del_last_task()
+
+# yea main func
+def main_func():
+    run_state = True
+    lock_key = False
+    lock_keyx = False
+    lock_keyy = False
+    while(run_state):
+        with open('memory.json') as f:
+            data = json.load(f)
+        nt = len(data['tasks'])
+        
+        if lock_key : clear_screen()
+
+        
+        print_logo()
+        print('Print Todo list [1]     Add task [2]      Complete task [3/all]    Quit [q]\n')
+
+
+        if lock_key : 
+            print()
+            task_viewer()
+            print()
+            lock_key = False
+
+        if lock_keyx :
+            if nt: 
+                print()
+                task_viewer()
+            if nt == 0:
+                print('There are no tasks to complete :(\n')
+            else:
+                deleter_task()
+                clear_screen()
+            lock_keyx = False
+        if not lock_keyx:
             ans = input("What do you want to?: ")
-                  
-        if ans == "1":lock_key = True
-        elif ans == "2":
-            task = input(f"\n Task no #{nt+1} :  ")
-            task_adder(task)
-            os.system("clear")
-        elif ans == "3": lock_keyx = True
-        elif ans == "q": 
-            run_state = False
-            print()
-            print("Come by again <3")
-            print()
-            time.sleep(4)
-            os.system("clear")
+            while(len(ans) != 1) or ((ans<"1" or ans>"4") and (ans<"q" or ans > "q")):
+                ans = input("What do you want to?: ")
+                    
+            if ans == "1":lock_key = True
+            elif ans == "2":
+                task = input(f"\n Task no #{nt+1} :  ")
+                task_adder(task)
+                clear_screen()
+            elif ans == "3": lock_keyx = True
+            elif ans == "q": 
+                run_state = False
+                print()
+                print("Come by again <3")
+                print()
+                sleep(4)
+                clear_screen()
+
+# yay program running
+main_func()
